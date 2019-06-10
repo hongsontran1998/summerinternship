ee763796a963487c9332888d4465d83c3d1aaa3a
tranhuuhongson <tranhuuhongson@gmail.com>
2019-06-10 08:24:35 +0700

    reactive programming in java

--- src/main/java/com/demo/javacore/Main.java
+++ src/main/java/com/demo/javacore/Main.java
@@ -1,13 +1,13 @@
 package com.demo.javacore;
 
 import com.demo.entity.Category;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
 
 public class Main {
     public static void main(String[] args) {
-        ObjectMapper mapper = new ObjectMapper();
+        //mapper trong jackson json
+        /*ObjectMapper mapper = new ObjectMapper();
         try {
             //json to object
             Category category = mapper.readValue("{\"id\" : \"1\", \"name\" : \"name\"}", Category.class);
@@ -17,6 +17,21 @@ public class Main {
             System.out.println(json);
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
+
+        //reactive programming in java
+        Observable<Integer> b = Observable.just(2, 5);
+        final int c = 3;
+
+        Consumer<Integer> consumer = new Consumer<Integer>() {
+
+            int a = 0;
+
+            public void accept(Integer number) throws Exception {
+                a = number + c;
+                System.out.println(a);
+            }
+        };
+
     }
 }
