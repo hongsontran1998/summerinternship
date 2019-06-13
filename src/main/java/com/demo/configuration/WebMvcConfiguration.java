1733a783e4c37f0353ce017647fc43b5ebf9ddaa
tranhuuhongson <tranhuuhongson@gmail.com>
2019-06-13 10:53:49 +0700

    web mvc config: interceptor

--- src/main/java/com/demo/configuration/WebMvcConfiguration.java
+++ src/main/java/com/demo/configuration/WebMvcConfiguration.java
@@ -1,7 +1,7 @@
 package com.demo.configuration;
 
 
-import com.demo.interceptor.ApplicationInterceptor;
+import com.demo.interceptor.DemoInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,21 +11,11 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        /*// LogInterceptor áp dụng cho mọi URL.
-        registry.addInterceptor(new LogInterceptor());
 
-        // Đường dẫn login cũ, không còn sử dụng nữa.
-        // Sử dụng OldURLInterceptor để điều hướng tới một URL mới.
-        registry.addInterceptor(new OldLoginInterceptor())//
-                .addPathPatterns("/admin/oldLogin");
-
-        // Interceptor này áp dụng cho các URL có dạng /admin/*
-        // Loại đi trường hợp /admin/oldLogin
-        registry.addInterceptor(new AdminInterceptor())//
-                .addPathPatterns("/admin/*")//
-                .excludePathPatterns("/admin/oldLogin");*/
-        registry.addInterceptor(new ApplicationInterceptor())
-                .addPathPatterns("/*"); //
+//        registry.addInterceptor(new ApplicationInterceptor())
+////                .addPathPatterns("/*");
+        registry.addInterceptor(new DemoInterceptor())
+                .addPathPatterns("/demo");
     }
 
 }
