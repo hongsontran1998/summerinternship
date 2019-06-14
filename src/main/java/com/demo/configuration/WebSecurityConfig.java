49774db7d2d70272524ff9971e731a5d6750b52d
tranhuuhongson <tranhuuhongson@gmail.com>
2019-06-14 11:13:53 +0700

    spring security

---
+++ src/main/java/com/demo/configuration/WebSecurityConfig.java
@@ -0,0 +1,106 @@
+package com.demo.configuration;
+
+import com.demo.security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.sql.DataSource;
+
+//https://spring.io/guides/gs/securing-web/
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+        BCryptPasswordEncoder encoder = passwordEncoder();
+        //create user for demo
+        auth.
+                inMemoryAuthentication()
+                    .withUser("admin")
+                    .password(encoder.encode("123"))
+                    .roles("ADMIN")
+                    .and()
+                    .passwordEncoder(encoder);
+        //authenticate from database
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+//        super.configure(web);
+        web.ignoring().antMatchers("/css/**", "/js/**", "/images/**");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+        http.authorizeRequests().antMatchers("/demo").access("hasRole('ROLE_ADMIN')");
+        http.authorizeRequests().antMatchers("/abc/**").access("hasAnyRole('ROLE_AMDIN', 'ROLE_USER')");
+        http.authorizeRequests()
+                .antMatchers("/api/*", "/logout", "/login").permitAll();
+        //những trang còn lại được bảo vệ
+        //http.authorizeRequests().anyRequest().authenticated();
+        //những trang còn lại ai vào cũng được vd ở phần public
+        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+        http
+            .cors().and()/*rest api*/
+            .csrf().and() //csrf
+            //.httpBasic() //cách nhanh không dùng form như ở dưới
+            .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/demo", false) //false: always-use-default-target="false"
+                .failureUrl("/login?error=loginErr")
+//                .successHandler()
+//                .failureHandler()
+                .and()
+            .logout()
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID");
+
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
