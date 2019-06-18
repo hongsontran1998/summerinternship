fd41ed2a5c81f5b862e66ad85eb13c7043dc7f8d
tranhuuhongson <tranhuuhongson@gmail.com>
2019-06-18 10:45:41 +0700

    using logging

--- src/main/java/com/demo/controller/AuthController.java
+++ src/main/java/com/demo/controller/AuthController.java
@@ -1,21 +1,46 @@
 package com.demo.controller;
 
-import com.demo.entity.User;
+import com.demo.repository.TestDAO;
 import com.demo.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private TestDAO testDAO;
+
+    @Autowired
+    private HttpServletRequest request;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
     @GetMapping("login")
     public String login() {
 //        System.out.println(new BCryptPasswordEncoder().encode("123"));
-        User u = userRepository.findById(1).get();
+//        User u = userRepository.findById(1).get();
 //        List<User> users = (List<User>) userRepository.findAll();
+//        List<Category> list = testDAO.test();
+//        LOGGER.trace("This is TRACE");
+//        LOGGER.debug("This is DEBUG");
+        LOGGER.info("Login infomation: ");
+        LOGGER.info("getRemoteAddr: " + request.getRemoteAddr());
+        LOGGER.info("getLocalAddr: " + request.getLocalAddr());
+        LOGGER.info("getLocalPort: " + request.getLocalPort());
+        LOGGER.info("getLocalName: " + request.getLocalName());
+        LOGGER.info("getRemoteUser: " + request.getRemoteUser());
+        LOGGER.info("getRemotePort: " + request.getRemotePort());
+//        LOGGER.warn("This is WARN");
+//        LOGGER.error("This is ERROR");
+
         return "auth/login";
     }
 }
