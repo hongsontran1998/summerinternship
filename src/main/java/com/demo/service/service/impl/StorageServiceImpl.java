b81c743adb6f5455caa5b90a37d150a6f3df3ce1
Son H.H. Tran <tranhuuhongson@gmail.com>
2019-08-23 18:58:05 +0700

    update Storage

--- src/main/java/com/demo/service/service/impl/StorageServiceImpl.java
+++ src/main/java/com/demo/service/service/impl/StorageServiceImpl.java
@@ -4,7 +4,6 @@ import com.demo.service.StorageProperties;
 import com.demo.service.StorageService;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
