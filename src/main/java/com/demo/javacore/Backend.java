7618d550409e3c9c988bde4ed887e6c51a8b29ea
Son H.H. Tran <tranhuuhongson@gmail.com>
2019-08-30 10:53:05 +0700

    update

--- src/main/java/com/demo/javacore/Backend.java
+++ src/main/java/com/demo/javacore/Backend.java
@@ -2,10 +2,20 @@ package com.demo.javacore;
 
 import com.demo.database.factory.JsonFactory;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Backend {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         JsonNode nodeCategories = JsonFactory.getJsonNode("categories");
+        System.out.println(nodeCategories.toString());
+
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayList<String> categoryNames = mapper.readValue(nodeCategories.toString(), ArrayList.class);
+        System.out.println(categoryNames);
+
         nodeCategories.forEach(item ->{
             System.out.println(item.asText());
             System.out.println(item.toString());
